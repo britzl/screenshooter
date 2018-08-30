0cc87dab75b460f08e3be3d802175d1b08b8ac34
Björn Ritzl <bjorn.ritzl@gmail.com>
2018-08-30 06:55:37 +0200

    First commit

---
+++ screenshooter/screenshooter.go
@@ -0,0 +1,30 @@
+components {
+  id: "gui"
+  component: "/screenshooter/screenshooter.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "screenshooter"
+  component: "/screenshooter/screenshooter.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
